e932cb6eafe1108693ea74e0452cdcabde6de434
Solpooh <hsw9420@naver.com>
2024-02-24 16:11:51 +0900

    [feat] typed.js 사용해 typing effect 생성

---
+++ main.js
@@ -0,0 +1,8 @@
+// typed.js => JS Library for Typing effect
+var typed = new Typed(".text", {
+    strings: ["Backend Developer", "Web Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
